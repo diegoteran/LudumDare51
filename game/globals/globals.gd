7deabe9926ffd3c6575800bbbd0bebad12663621
Diego Terán Ríos <diegoteran94@gmail.com>
2022-10-01 21:38:39 -0700

    woops

--- game/globals/globals.gd
+++ game/globals/globals.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var unlocked_rooms = {"a1": [], "a2": [], "a3": [], "h1": [], "h2": [],
-					  "b1": [], "b2": [], "b3": [], "c1": [], "c2": [], "c3": []}
+					  "b1": [], "b2": [], "b3": [], "c1": [], "c2": [], "c3": [],
+					  "d1": [], "d2": [], "d3": []}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
